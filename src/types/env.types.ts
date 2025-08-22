c9aef6f969b57b07b753d44c1630bce6eb53aa0c
Matheus Altrao <matheusaltrao2@gmail.com>
2025-08-22 00:16:28 -0400

    ✨ feat(env/types): Adiciona a variável `JWT_SECRET_KEY` ao tipo de ambiente global

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -4,6 +4,7 @@ declare global {
       ENVIRONMENT: "development" | "production" | "test";
       NEXT_PUBLIC_API_URL: string;
       NEXT_PUBLIC_BASE_URL: string;
+      JWT_SECRET_KEY: string;
     }
   }
 }
