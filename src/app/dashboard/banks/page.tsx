a16f6360e124fc23228dc0a5f2ac7a4733745bb5
Matheus Altrao <matheusaltrao2@gmail.com>
2025-08-23 23:40:21 -0400

    ✨ feat(dashboard/banks): Adiciona layout de contas bancárias com cards e botão para adicionar nova conta

--- src/app/dashboard/banks/page.tsx
+++ src/app/dashboard/banks/page.tsx
@@ -1,3 +1,91 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Plus, PlusCircle } from "lucide-react";
+
 export default function BanksPage() {
-  return <div>Banks Page</div>;
+  return (
+    <div className="space-y-8">
+      <div className="flex items-center justify-end">
+        <Button>
+          <Plus /> Adicionar conta
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <Card className="flex h-[220px] flex-col items-center justify-center bg-accent">
+          <CardContent className="flex flex-col items-center justify-center gap-3">
+            <PlusCircle size={64} strokeWidth={0.8} />
+            Nova conta bancária
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center gap-2">
+              <div className="h-8 w-8 rounded-full bg-primary" />
+              <span>Inter</span>
+            </div>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="flex items-center justify-between">
+              <span>Saldo atual</span>
+              <span className="text-muted-foreground">R$ 1.200,00</span>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button variant="secondary" className="w-full">
+              <Plus /> Adicionar dispesa
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center gap-2">
+              <div className="h-8 w-8 rounded-full bg-primary" />
+              <span>Inter</span>
+            </div>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="flex items-center justify-between">
+              <span>Saldo atual</span>
+              <span className="text-muted-foreground">R$ 1.200,00</span>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button variant="secondary" className="w-full">
+              <Plus /> Adicionar dispesa
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center gap-2">
+              <div className="h-8 w-8 rounded-full bg-primary" />
+              <span>Inter</span>
+            </div>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <div className="flex items-center justify-between">
+              <span>Saldo atual</span>
+              <span className="text-muted-foreground">R$ 1.200,00</span>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button variant="secondary" className="w-full">
+              <Plus /> Adicionar dispesa
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function App() {
-  return redirect("/auth/sign-in");
+  return redirect("/dashboard");
 }
