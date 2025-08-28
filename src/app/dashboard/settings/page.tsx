30e555d06583f807759aa4ac95deb66c6e0503e3
Matheus Altrao <matheusaltrao2@gmail.com>
2025-08-28 02:11:29 -0400

    ✨ feat(ui/theme): Adiciona o componente `ThemeButton` para seleção de temas e implementa o `ThemeProvider` no layout

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ThemeButton } from "@/components/ui/theme-button";
+
+export default function SettingsPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Configurações</CardTitle>
+        <CardDescription>
+          Selecione uma configuração de tema melhor para você{" "}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex items-center gap-2">
+          <span>Selecione o tema</span> <ThemeButton />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from "@/providers/theme";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -19,10 +19,17 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" suppressHydrationWarning>
       <body className={`${poppins.className} antialiased`}>
         <Toaster />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
