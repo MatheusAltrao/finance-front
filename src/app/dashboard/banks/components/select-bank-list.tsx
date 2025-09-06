e9ff1061623c654c8859b611317d8fe082c1dfd7
Matheus Altrao <matheusaltrao2@gmail.com>
2025-09-06 04:07:42 -0400

    ✨ feat(dashboard/banks): Adiciona o componente `SelectBankList` para seleção de bancos e configura formatação padrão para arquivos `typescriptreact` e `jsonc`

---
+++ src/app/dashboard/banks/components/select-bank-list.tsx
@@ -0,0 +1,88 @@
+/** biome-ignore-all lint/a11y/useButtonType: <explanation> */
+"use client";
+import { Button } from "@/components/ui/button";
+import Loading from "@/components/ui/loading";
+import type { BankProps } from "@/types/banks";
+import { Check } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import toast from "react-hot-toast";
+
+interface SelectBankListProps {
+  banks: BankProps[];
+  token:string
+}
+
+export default function SelectBankList({ banks,token }: SelectBankListProps) {
+  const router = useRouter();
+
+  const [selectedBank, setSelectedBank] = useState<BankProps | null>(null);
+  const [isPending,startTransition] = useTransition()
+
+  const handleBindBank = async () => {
+
+    if(!selectedBank) {
+      toast.error("Selecione um banco para continuar.");
+      return;
+    }
+
+    startTransition(async()=>{
+ try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/accounts/bind`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            account_id: selectedBank?.id,
+          }),
+        },
+      );
+
+      console.log(response);
+      toast.success("Banco vinculado com sucesso!");
+      setSelectedBank(null);
+      router.refresh()
+    } catch (error) {
+      console.log(error);
+      toast.error("Erro ao vincular banco. Tente novamente.");
+    }
+    })
+   
+  };
+
+  return (
+    <div className="space-y-4">
+      <div className="h-[350px] space-y-2 overflow-y-auto">
+        {banks.map((bank) => (
+          <button
+            onClick={() => setSelectedBank(bank)}
+            key={bank.id}
+            className={`flex w-full items-center gap-2 rounded-md border p-2 transition-colors hover:bg-accent ${
+              selectedBank?.id === bank.id
+                ? "border-primary bg-accent"
+                : "border-transparent"
+            }`}
+          >
+            <Image
+              className="rounded-full"
+              src={bank.logoUrl}
+              alt=""
+              width={40}
+              height={40}
+            />
+            {bank.name}
+          </button>
+        ))}
+      </div>
+
+      <Button  disabled={!selectedBank || isPending} onClick={handleBindBank}  className="w-full">
+       {isPending ? <Loading /> : <Check />}  Confirmar Escolha
+      </Button>
+    </div>
+  );
+}
--- src/app/dashboard/banks/page.tsx
+++ src/app/dashboard/banks/page.tsx
@@ -1,7 +1,10 @@
-import { Plus, PlusCircle } from 'lucide-react'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import {
   Dialog,
   DialogContent,
@@ -9,47 +12,54 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog'
-import { getSession } from '@/helpers/session'
-import type { BankProps, UserBankProps } from '@/types/banks'
+} from "@/components/ui/dialog";
+import { getSession } from "@/helpers/session";
+import type { BankProps, UserBankProps } from "@/types/banks";
+import { Plus, PlusCircle } from "lucide-react";
+import Image from "next/image";
+import SelectBankList from "./components/select-bank-list";
 
 async function getBankList() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/accounts`)
-    const data = await response.json()
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/accounts`);
+    const data = await response.json();
 
-    return data as BankProps[]
+    return data as BankProps[];
   } catch (error) {
-    console.log(error)
-    return []
+    console.log(error);
+    return [];
   }
 }
 
 async function getUserBank(token: string) {
   if (!token) {
-    throw new Error('Token is required')
+    throw new Error("Token is required");
   }
 
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/accounts`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/accounts`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       },
-    })
+    );
 
-    const data = await response.json()
+    const data = await response.json();
 
-    return data as UserBankProps[]
+    return data as UserBankProps[];
   } catch (error) {
-    console.log(error)
-    return []
+    console.log(error);
+    return [];
   }
 }
 
 export default async function BanksPage() {
-  const session = await getSession()
-  const bankList = await getBankList()
-  const userBanks = await getUserBank(session?.value || '')
+  const session = await getSession();
+  const token = session?.value!;
+  const bankList = await getBankList();
+  const userBanks = await getUserBank(session?.value || "");
 
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
@@ -67,31 +77,26 @@ export default async function BanksPage() {
           <DialogHeader>
             <DialogTitle>Adicionar uma conta bancária</DialogTitle>
             <DialogDescription>
-              Aqui você pode adicionar uma nova conta bancária para gerenciar suas finanças.
+              Aqui você pode adicionar uma nova conta bancária para gerenciar
+              suas finanças.
             </DialogDescription>
           </DialogHeader>
 
-          <div>
-            <ul className="h-[420px] space-y-2 overflow-y-auto">
-              {bankList.map((bank) => (
-                <li
-                  key={bank.id}
-                  className="flex cursor-pointer items-center gap-2 rounded-md border p-2 transition-colors hover:bg-accent"
-                >
-                  <Image className="rounded-full" src={bank.logoUrl} alt="" width={40} height={40} />
-                  {bank.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+          <SelectBankList banks={bankList} token={token} />
         </DialogContent>
       </Dialog>
 
-      {userBanks.map((bank, index) => (
+      {userBanks.map((bank) => (
         <Card key={`${bank.id}`}>
           <CardHeader>
             <div className="flex items-center gap-2">
-              <Image className="rounded-full" src={bank.logo} alt={bank.name} width={32} height={32} />
+              <Image
+                className="rounded-full"
+                src={bank.logo}
+                alt={bank.name}
+                width={32}
+                height={32}
+              />
               <span>{bank.name}</span>
             </div>
           </CardHeader>
@@ -110,5 +115,5 @@ export default async function BanksPage() {
         </Card>
       ))}
     </div>
-  )
+  );
 }
