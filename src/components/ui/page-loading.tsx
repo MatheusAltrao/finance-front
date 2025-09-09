74dce5d41e6d52ffc8e585b54e8efec0324dbf31
Matheus Altrao <matheusaltrao2@gmail.com>
2025-09-09 00:08:24 -0400

    ✨ feat(quotes/loading): Adiciona componentes de carregamento `QuotesLoading` e `PageLoading` para melhorar a experiência do usuário durante o carregamento dos dados

---
+++ src/components/ui/page-loading.tsx
@@ -0,0 +1,9 @@
+import Loading from './loading'
+
+export default function PageLoading() {
+  return (
+    <div className="flex items-center h-[80%]  justify-center">
+      <Loading />
+    </div>
+  )
+}
