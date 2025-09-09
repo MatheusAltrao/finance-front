8f68f126daeab0cbc1aace367b727b69e25176b4
Matheus Altrao <matheusaltrao2@gmail.com>
2025-09-08 23:56:32 -0400

    ✨ feat(stocks): Adiciona a função `getStocksAction` para buscar e formatar dados de ações e moedas

--- src/app/dashboard/quotes/page.tsx
+++ src/app/dashboard/quotes/page.tsx
@@ -1,30 +1,9 @@
-import { formatCurrencies, formatStocks } from '@/helpers/format-quotes'
-import type { QuoteProps, StocksProps } from '@/types/quotes'
+import { getStocksAction } from '@/actions/stocks/get-stocks-action'
 import { QuotesCurrencyCard } from './components/quotes-currency-card'
 import { QuotesStockCard } from './components/quotes-stock-card'
 
-async function fetchStocks() {
-  try {
-    const response = await fetch(`https://api.hgbrasil.com/finance`, {
-      next: { revalidate: 60 }, // 1 minuto
-    })
-
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.log(error)
-    return []
-  }
-}
-
 export default async function QuotesPage() {
-  const quotes: QuoteProps = await fetchStocks()
-  console.log(quotes)
-  const currencies = quotes.results.currencies
-  const stocks = quotes.results.stocks
-
-  const formattedCurrencies = formatCurrencies(currencies)
-  const formattedStocks: StocksProps[] = formatStocks(stocks)
+  const { currencies, stocks } = await getStocksAction()
 
   return (
     <div className="flex flex-col gap-8">
@@ -36,7 +15,7 @@ export default async function QuotesPage() {
           </p>
         </div>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-          {formattedCurrencies.map((currency) => (
+          {currencies.map((currency) => (
             <QuotesCurrencyCard key={currency.name} data={currency} />
           ))}
         </div>
@@ -49,7 +28,7 @@ export default async function QuotesPage() {
           </p>
         </div>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-          {formattedStocks.map((stock) => (
+          {stocks.map((stock) => (
             <QuotesStockCard key={stock.name} data={stock} />
           ))}
         </div>
